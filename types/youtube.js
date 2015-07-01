3368478d151d295a1b306ee640618c4d1d66dd06
Chiel Kunkels <chiel@kunkels.me>
2015-07-01 22:50:20 +0200

    youtube: use new fit mechanic

--- types/youtube.js
+++ types/youtube.js
@@ -3,17 +3,22 @@
 var Darkbox = require('..');
 
 Darkbox.types.youtube = function(opts){
+	var self = this;
 	var width = opts.width || 560;
 	var height = opts.height || 315;
-	var size = this.fit(width, height);
 
-	var iframe = document.createElement('iframe');
-	iframe.width = size.width;
-	iframe.height = size.height;
-	iframe.src = 'https://www.youtube.com/embed/' + opts.id;
-	iframe.setAttribute('frameborder', 0);
-	iframe.setAttribute('allowfullscreen', '');
+	self.empty();
+	self.fit(width, height, {
+		callback: function(){
+			var iframe = document.createElement('iframe');
+			iframe.width = width;
+			iframe.height = height;
+			iframe.src = 'https://www.youtube.com/embed/' + opts.id;
+			iframe.setAttribute('frameborder', 0);
+			iframe.setAttribute('allowfullscreen', '');
 
-	this.empty();
-	this.content.appendChild(iframe);
+			self.content.appendChild(iframe);
+			self.content.classList.add('is-loaded');
+		}
+	});
 };
